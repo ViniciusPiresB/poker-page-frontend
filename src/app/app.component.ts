b093955448ea82aa5162114283359b433bcaccc9
Erik Mota <seriikmota.dev@gmail.com>
2024-04-15 17:39:51 -0300

    Estilizado página padrão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {NgOptimizedImage} from "@angular/common";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, NgOptimizedImage, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', loadChildren: () => import('./features/user/user.module').then(u => u.UserModule)},
+];
